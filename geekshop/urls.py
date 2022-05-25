cc2469f08b1ec0b56148ab40beb33c938b89b107
zzollozz <81852084+zzollozz@users.noreply.github.com>
2022-05-25 19:48:25 +0300

    выполнил ДЗ по 2 занятию

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -20,7 +20,14 @@ from mainapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('', views.index),
-    path('products.html/', views.products),
-    path('contact.html/', views.contact),
+    path('', views.index, name='index'),
+
+    path('products/', views.products, name='products'),
+    path('products/all/', views.products, name='products_all'),
+    path('products/home/', views.products, name='products_home'),
+    path('products/office/', views.products, name='products_office'),
+    path('products/modern/', views.products, name='products_modern'),
+    path('products/classic/', views.products, name='products_classic'),
+
+    path('contact/', views.contact, name='contact'),
 ]
