bbac3518c0ff88d6b4a303b9a5320e833f942e66
zzollozz <81852084+zzollozz@users.noreply.github.com>
2022-09-15 10:38:50 +0300

    внес изменения в settings STATIC_ROOT

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -170,9 +170,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = (
-    BASE_DIR / 'static',
-)
+# STATICFILES_DIRS = (
+#     BASE_DIR / 'static',
+# )
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
